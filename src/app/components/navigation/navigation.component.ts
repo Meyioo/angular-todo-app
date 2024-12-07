b91a3569dea48ab0c0bd01cfe5a640ed49885cf0
Meyioo <tim.meyerhoff@outlook.de>
2024-12-07 10:35:39 +0100

    Enhance navigation component with routerLink and active link styling

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { TodoService } from '../../service/todo.service';
 
 @Component({
@@ -7,8 +8,17 @@ import { TodoService } from '../../service/todo.service';
 	templateUrl: './navigation.component.html',
 	styleUrls: ['./navigation.component.css'],
 	standalone: true,
-	imports: [CommonModule]
+	imports: [CommonModule, RouterLink]
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+	public readonly activatedRoute = inject(ActivatedRoute);
+	public readonly router = inject(Router);
+
 	public readonly todoService = inject(TodoService);
+
+	ngOnInit(): void {
+		this.activatedRoute.url.subscribe((url) => {
+			console.log(url);
+		});
+	}
 }
